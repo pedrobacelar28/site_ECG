e17d8c62a082c9f0bb3b7b150f21506bceffbc68
Guilherme H <guihenri2004@hotmail.com>
2024-09-24 23:55:56 -0300

    index tmj

---
+++ api/prisma/migrations/20240925025030_new3/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `dateRecPass` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `tokenRecPass` on the `User` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "senha" TEXT NOT NULL,
+    "cargo" TEXT NOT NULL
+);
+INSERT INTO "new_User" ("cargo", "email", "id", "nome", "senha") SELECT "cargo", "email", "id", "nome", "senha" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
